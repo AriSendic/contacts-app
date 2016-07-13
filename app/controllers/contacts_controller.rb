178c8549af06f2f7a60b1b2dc2957b3d52dde68b
AriSendic <rabbisendic@gmail.com>
2016-07-13 13:47:20 -0500

    initial commit, no flash or bootstrap

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,51 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    render 'index.html.erb'
+  end
+  
+  def new
+    @contact = Contact.new
+    render "new.html.erb"
+  end
+
+  def create
+    contact = Contact.new(
+     :first_name => params['first_name'],
+     :last_name => params['last_name'],
+     :phone_number => params['phone_number'],
+     :email => params['email']
+    )
+    contact.save
+    redirect_to '/contacts'
+  end
+  
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    
+    render 'show.html.erb'
+  end
+  
+  def edit
+    @contact = Contact.find_by(id: params['id'])
+    render 'edit.html.erb'
+  end
+  
+  def update
+    contact = Contact.find_by(id: params['id'])
+      contact.update(
+      first_name: params['first_name'],
+      last_name: params['last_name'],
+      email: params['email'],
+      phone_number: params['phone_number']
+    )
+    
+    redirect_to "/contacts/#{contact.id}"
+  end
+
+  def destroy
+    contact = Contact.find_by(id: params['id'])
+    contact.destroy
+    redirect_to "/contacts"
+  end
+end

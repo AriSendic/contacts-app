62416bbbc51dac76a2929c4af1f0570c2c8380fa
AriSendic <rabbisendic@gmail.com>
2016-07-14 15:49:26 -0500

    added flash, link to new, and styles

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,18 +11,18 @@ class ContactsController < ApplicationController
 
   def create
     contact = Contact.new(
-     :first_name => params['first_name'],
-     :last_name => params['last_name'],
-     :phone_number => params['phone_number'],
-     :email => params['email']
+      first_name: params['first_name'],
+      last_name: params['last_name'],
+      phone_number: params['phone_number'],
+      email: params['email']
     )
     contact.save
+    flash[:success] = "Contact successfuly added"
     redirect_to '/contacts'
   end
   
   def show
-    @contact = Contact.find_by(id: params[:id])
-    
+    @contact = Contact.find_by(id: params['id'])    
     render 'show.html.erb'
   end
   
@@ -33,19 +33,20 @@ class ContactsController < ApplicationController
   
   def update
     contact = Contact.find_by(id: params['id'])
-      contact.update(
+    contact.update(
       first_name: params['first_name'],
       last_name: params['last_name'],
       email: params['email'],
       phone_number: params['phone_number']
     )
-    
+     flash[:success] = "Contact successfully updated"
     redirect_to "/contacts/#{contact.id}"
   end
 
   def destroy
     contact = Contact.find_by(id: params['id'])
     contact.destroy
+    flash[:success] = "Product successfuly deleted"
     redirect_to "/contacts"
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,14 @@
 class Contact < ActiveRecord::Base
+  def friendly_time
+    created_at.strftime('%A, %b %d')
+  end
+
+  def full_name
+    first_name + " " + last_name
+  end  
+  
+  def japan_number
+    "+81" + phone_number
+  end   
+
 end
